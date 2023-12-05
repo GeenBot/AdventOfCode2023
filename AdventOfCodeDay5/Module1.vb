f4999d39e1fd82436571e55b7cac34f8ff3f1cb6
Xander Hawkins <Alexander0Hawkins@gmail.com>
2023-12-05 17:38:29 +0000

    Part 1 of AOC Day 5 Done

---
+++ AdventOfCodeDay5/Module1.vb
@@ -0,0 +1,193 @@
+﻿Imports System.IO
+
+Module Module1
+
+
+    'Provides the range with which to compare
+    Structure Map
+        Dim destinationStartValue As Double
+        Dim sourceStartValue As Double
+        Dim destinationEndValue As Double
+        Dim sourceEndValue As Double
+    End Structure
+
+
+    Sub Main()
+        Dim fileReader As StreamReader = My.Computer.FileSystem.OpenTextFileReader("C:\Users\Xander\source\repos\AdventOfCode2023\TextInputs\Day5Input.txt")
+        Dim textInput As String = fileReader.ReadLine()
+        Dim seedString = textInput.Remove(0, 7)
+        Dim numberArray(7, 19) As String
+        Dim seedArray(19) As String
+        Dim counter As Integer = 0
+        Dim valueRange(60) As Map
+
+        'Splits the string into an array of number and then adds it to one dimension of an array using a for loop since I don't know how to otherwise
+        seedArray = Split(seedString, " ")
+        For i = 0 To seedArray.Length - 1
+            numberArray(0, i) = seedArray(i)
+        Next
+
+
+        'Goes to the beginning of the first Map
+        textInput = fileReader.ReadLine()
+        textInput = fileReader.ReadLine()
+        textInput = fileReader.ReadLine()
+
+        Do
+            Dim i = 0
+            Do
+                'Makes one range of values for which to compare with
+                valueRange(i) = FillRange(textInput)
+                textInput = fileReader.ReadLine()
+                i += 1
+                'Repeats for every line in the map
+            Loop Until textInput = ""
+            'Compares the currently held number against the ranges of values
+            numberArray = FindValue(valueRange, numberArray, counter)
+
+            'increments the stage at which you are looking before heading to the next map
+            counter += 1
+            textInput = fileReader.ReadLine()
+            textInput = fileReader.ReadLine()
+
+            'Repeats for every map
+        Loop Until textInput = ""
+
+        'prints out the values in the final stage
+        For i = 0 To numberArray.GetLength(1) - 1
+            Console.WriteLine(numberArray(7, i))
+        Next
+        Console.ReadLine()
+    End Sub
+
+    'Compares the number to see whether it is in the range and then gives it the the correct new value based on whether it is or not
+    Function FindValue(valRange() As Map, num(,) As String, counter As Integer)
+        For i = 0 To num.GetLength(1) - 1
+            For a = 0 To valRange.Length - 1
+                If num(counter, i) >= valRange(a).sourceStartValue And num(counter, i) <= valRange(a).sourceEndValue Then
+                    num(counter + 1, i) = num(counter, i) - valRange(a).sourceStartValue + valRange(a).destinationStartValue
+                    Exit For
+                ElseIf a = valRange.Length - 1 Then
+                    num(counter + 1, i) = num(counter, i)
+                End If
+            Next
+
+        Next
+        Return num
+    End Function
+
+
+    'Calculates the ranges by adding the base range to the two starting values
+    Function FillRange(textInput As String)
+        Dim rangeParts(2) As String
+        Dim valueRange As Map
+        Dim range(2) As Double
+        rangeParts = Split(textInput, " ")
+        For i = 0 To 2
+            range(i) = Val(rangeParts(i))
+        Next
+
+        valueRange.destinationStartValue = range(0)
+        valueRange.destinationEndValue = range(0) + range(2)
+        valueRange.sourceStartValue = range(1)
+        valueRange.sourceEndValue = range(1) + range(2)
+        Return valueRange
+    End Function
+
+End Module
+
+Module Module2
+
+
+    'Provides the range with which to compare
+    Structure Map
+        Dim destinationStartValue As Double
+        Dim sourceStartValue As Double
+        Dim destinationEndValue As Double
+        Dim sourceEndValue As Double
+    End Structure
+
+
+    Sub Main()
+        Dim fileReader As StreamReader = My.Computer.FileSystem.OpenTextFileReader("C:\Users\Xander\source\repos\AdventOfCode2023\TextInputs\Day5Input.txt")
+        Dim textInput As String = fileReader.ReadLine()
+        Dim seedString = textInput.Remove(0, 7)
+        Dim numberArray(7, 19) As String
+        Dim seedArray(19) As String
+        Dim counter As Integer = 0
+        Dim valueRange(60) As Map
+
+        'Splits the string into an array of number and then adds it to one dimension of an array using a for loop since I don't know how to otherwise
+        seedArray = Split(seedString, " ")
+        For i = 0 To seedArray.Length - 1
+            numberArray(0, i) = seedArray(i)
+        Next
+
+
+        'Goes to the beginning of the first Map
+        textInput = fileReader.ReadLine()
+        textInput = fileReader.ReadLine()
+        textInput = fileReader.ReadLine()
+
+        Do
+            Dim i = 0
+            Do
+                'Makes one range of values for which to compare with
+                valueRange(i) = FillRange(textInput)
+                textInput = fileReader.ReadLine()
+                i += 1
+                'Repeats for every line in the map
+            Loop Until textInput = ""
+            'Compares the currently held number against the ranges of values
+            numberArray = FindValue(valueRange, numberArray, counter)
+
+            'increments the stage at which you are looking before heading to the next map
+            counter += 1
+            textInput = fileReader.ReadLine()
+            textInput = fileReader.ReadLine()
+
+            'Repeats for every map
+        Loop Until textInput = ""
+
+        'prints out the values in the final stage
+        For i = 0 To numberArray.GetLength(1) - 1
+            Console.WriteLine(numberArray(7, i))
+        Next
+        Console.ReadLine()
+    End Sub
+
+    'Compares the number to see whether it is in the range and then gives it the the correct new value based on whether it is or not
+    Function FindValue(valRange() As Map, num(,) As String, counter As Integer)
+        For i = 0 To num.GetLength(1) - 1
+            For a = 0 To valRange.Length - 1
+                If num(counter, i) >= valRange(a).sourceStartValue And num(counter, i) <= valRange(a).sourceEndValue Then
+                    num(counter + 1, i) = num(counter, i) - valRange(a).sourceStartValue + valRange(a).destinationStartValue
+                    Exit For
+                ElseIf a = valRange.Length - 1 Then
+                    num(counter + 1, i) = num(counter, i)
+                End If
+            Next
+
+        Next
+        Return num
+    End Function
+
+
+    'Calculates the ranges by adding the base range to the two starting values
+    Function FillRange(textInput As String)
+        Dim rangeParts(2) As String
+        Dim valueRange As Map
+        Dim range(2) As Double
+        rangeParts = Split(textInput, " ")
+        For i = 0 To 2
+            range(i) = Val(rangeParts(i))
+        Next
+
+        valueRange.destinationStartValue = range(0)
+        valueRange.destinationEndValue = range(0) + range(2)
+        valueRange.sourceStartValue = range(1)
+        valueRange.sourceEndValue = range(1) + range(2)
+        Return valueRange
+    End Function
+
+End Module
---
+++ AdventOfCodeDay5/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("AdventOfCodeDay5")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("AdventOfCodeDay5")>
+<Assembly: AssemblyCopyright("Copyright ©  2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("e02c8574-6df1-4a98-aeaf-fad3f9d66f28")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
